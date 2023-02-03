803dfbb2b1ad0ba1a4d8e87c413f256c5febb857
Gerson <byforevercs@gmail.com>
2023-02-03 12:02:17 -0300

    avance de todo's

--- scr/store/todo.store.js
+++ scr/store/todo.store.js
@@ -22,6 +22,70 @@ const initStore = () => {
     console.log('InitStore');
 }
 
+const loadStore = () =>{
+    throw new Error('Not implemented');
+}
+/**
+ * 
+ * @param {string} description Agrega un todo a la lista
+ */
+const addTodo = (description) => {
+
+    if(!description) throw new Error('Description is required');
+    state.todos.push(new Todo(description));
+}
+const getTodo = (filter = Filters.All) => {
+    switch(filter){
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+        }
+
+}
+/**
+ * cambia los todos de completado o no
+ * @param {string} todoId  
+ */
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map(todo => {
+        if(todo.id === todoId){
+            todo.done =! todo.done;
+        }
+        return todo;
+    });
+}
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
+}
+
+const deleteCompleted = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.done);
+}
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFliter = (newFilter = Filters.All) => {
+    state.filter = newFilter();
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFliter,
+    getTodo,
+    toggleTodo,
 }
--- scr/todos/app.js
+++ scr/todos/app.js
@@ -1,16 +1,45 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './use-cases';
 /**
  * 
  * @param {String} elementId elemento que renderiza la app 
  */
 
+const ElementIDs = {
+    TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input'
+}
+
 export const App = (elementId) =>{
+
+    const displayTodos = () =>{
+        const todos = todoStore.getTodo(todoStore.getCurrentFilter());
+        renderTodos(ElementIDs.TodoList, todos);
+    }
+
 
     //Cuando la App() se llama [función autoinvocada]
     (()=>{
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
+
+    //Referencias HTML -> Aca ya que antes no se habian creado
+
+    const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput);
+
+    //Listener
+    newDescriptionInput.addEventListener('keyup', (event)=> {
+        if(event.keyCode !== 13) return; //Cualquier tecla de precione no continuará la ejecucion a menos que sea Enter (13)
+        if(event.target.value.trim().length === 0) return; //Si esta vacio no hará nada
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value = '';
+    });
+
+
 
 }
